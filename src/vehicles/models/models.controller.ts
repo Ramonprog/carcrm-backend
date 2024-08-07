8f00c6c79894811db8bd6cc4db48670c2f7b8ce4
Ramonprog <programador.ramon@gmail.com>
2024-08-07 20:42:07 -0300

    feat:salvando

--- src/vehicles/models/models.controller.ts
+++ src/vehicles/models/models.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ModelsService } from './models.service';
-import { CreateModelDto } from './dto/create-model.dto';
-import { UpdateModelDto } from './dto/update-model.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common'
+import { ModelsService } from './models.service'
+import { CreateModelDto } from './dto/create-model.dto'
+import { UpdateModelDto } from './dto/update-model.dto'
+import { AuthGuard } from 'src/auth/auth.guard'
 
 @Controller('models')
 export class ModelsController {
@@ -9,26 +19,26 @@ export class ModelsController {
 
   @Post()
   create(@Body() createModelDto: CreateModelDto) {
-    return this.modelsService.create(createModelDto);
+    return this.modelsService.create(createModelDto)
   }
-
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
-    return this.modelsService.findAll();
+    return this.modelsService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.modelsService.findOne(+id);
+    return this.modelsService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateModelDto: UpdateModelDto) {
-    return this.modelsService.update(+id, updateModelDto);
+    return this.modelsService.update(+id, updateModelDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.modelsService.remove(+id);
+    return this.modelsService.remove(+id)
   }
 }
--- src/vehicles/models/models.service.ts
+++ src/vehicles/models/models.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { CreateModelDto } from './dto/create-model.dto';
-import { UpdateModelDto } from './dto/update-model.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateModelDto } from './dto/create-model.dto'
+import { UpdateModelDto } from './dto/update-model.dto'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class ModelsService {
+  constructor(private readonly prismaService: PrismaService) {}
   create(createModelDto: CreateModelDto) {
-    return 'This action adds a new model';
+    return 'This action adds a new model'
   }
 
   findAll() {
-    return `This action returns all models`;
+    return this.prismaService.vehicleModel.findMany()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} model`;
+    return `This action returns a #${id} model`
   }
 
   update(id: number, updateModelDto: UpdateModelDto) {
-    return `This action updates a #${id} model`;
+    return `This action updates a #${id} model`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} model`;
+    return `This action removes a #${id} model`
   }
 }
